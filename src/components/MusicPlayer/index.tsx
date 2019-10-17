48fe52c69f354a19a0df28d350c83587823b7832
Billy Irianto <billy_irianto@yahoo.com>
2019-10-17 20:38:24 +0700

    useRef for audio html5 control

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -1,65 +1,59 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import moment from 'moment'
 import './musicplayer.css'
+import playbutton from './play-button.svg'
+import pausebutton from './pause-button.svg'
 const testsong = require('./Lemon.mp3')
 const testsong2 = require('./mainactor.mp3')
 
 const MusicPlayer: React.FC = () => {
-  const audio: HTMLAudioElement = document.createElement('audio');
-  const [currentTime, setCurrentTime] = useState<number>(0)
-  const [duration, setDuration] = useState<number>(0)
-  const [song, setSong] = useState<string>("")
-  // const [status, setStatus] = useState<string>("paused")
 
-  audio.src=song
-  audio.onloadedmetadata = () => {
-    setDuration(audio.duration)
-    // audio.addEventListener("timeupdate", handlePlayTimeUpdate)
+  const player = useRef<HTMLAudioElement>(new Audio)
+  let [currentTime, setCurrentTime] = useState<number>(-1)
+  let [duration, setDuration] = useState<number>(-1)
+  let [song, setSong] = useState<string>("")
+
+  player.current.oncanplaythrough = (e) => {
+    let audioObject = e.target as HTMLAudioElement
+    setCurrentTime(audioObject.currentTime)
+    setDuration(audioObject.duration)
   };
 
-  const handlePlayTimeUpdate = (e: Event) => {
-    let audioTarget = e.target as HTMLAudioElement
-    setCurrentTime(audioTarget.currentTime)
-  };
-
-  setTimeout(() => {
-    setDuration(audio.duration)
-  }, 10000)
-
-  // useEffect(() => {
-  //   audio.addEventListener("onprogress", handlePlayTimeUpdate)
-  // }, [])
+  player.current.ontimeupdate = (e) => {
+    let audioObject = e.target as HTMLAudioElement
+    console.log(audioObject.currentTime)
+    setCurrentTime(audioObject.currentTime)
+  }
 
   const onPlay = () => {
-    audio.play()
+    player.current.play()
   }
-  const onStop = async() => {
-    await audio.pause()
-    await audio.removeEventListener("timeupdate", ()=>{})
+  
+  const onPause = () => {
+    player.current.pause()
   }
 
   const onAudioSelect = async (url: string) => {
-    await audio.pause()
-    await audio.load()
-    await setSong(url)
-    setDuration(audio.duration)
-    setCurrentTime(audio.currentTime)
+    setSong(url)
   }
   
   return (
-    <div style={{color:"#eeeeee", backgroundColor:"#222831"}}>
+    <div style={{color:"#eeeeee", backgroundColor:"#222831"}} className="d-flex  flex-column align-items-center">
       This is an audio player
+      <div>{!song ? "No song selected" : "Song selected"}</div>
       <button onClick={() => onAudioSelect(testsong)}>audio 1</button>
       <button onClick={() => onAudioSelect(testsong2)}>audio 2</button>
-      <audio src={testsong} id="music-player"/>
-      <div style={{width: "100%", height: "100px", backgroundColor:"#393e46"}}>
+      <audio ref={player} src={song} id="music-player"/>
+      <div style={{width: "100%", height: "100px", backgroundColor:"#393e46"}} className="d-flex flex-column align-items-center">
         <div className="d-flex flex-row"  style={{color:"#eeeeee"}}>
-          <div>{moment(currentTime*1000).format("mm:ss")}</div>
+          <div>{currentTime >= 0 ? moment(currentTime*1000).format("mm:ss") : "--:--"}</div>
           <div>/</div>
-          <div>{moment(duration*1000).format("mm:ss")}</div>
+          <div>{duration >=0 ? moment(duration*1000).format("mm:ss") : "--:--"}</div>
         </div>
-        <div onClick={onPlay} className="player-button">Play</div>
-        <div onClick={onStop} className="player-button">Pause</div>
+        <div className="d-flex flex-row">
+        <img src={playbutton} onClick={onPlay} className="player-button" alt="play button"/>
+        <img src={pausebutton} onClick={onPause} className="player-button" alt="pause button"/>
+        </div>
       </div>
     </div>
   );
