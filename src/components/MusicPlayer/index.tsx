c991aee388a0cc03684ab49478880e5f24c538f7
Billy Irianto <billy_irianto@yahoo.com>
2019-10-18 08:33:59 +0700

    testing slider

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useRef} from 'react';
 import moment from 'moment'
 import './musicplayer.css'
 import playbutton from './play-button.svg'
@@ -6,35 +6,59 @@ import pausebutton from './pause-button.svg'
 const testsong = require('./Lemon.mp3')
 const testsong2 = require('./mainactor.mp3')
 
+// const Seeker = () => {
+
+// }
+
 const MusicPlayer: React.FC = () => {
+  const player = useRef<HTMLAudioElement>(new Audio())
+  const [currentTime, setCurrentTime] = useState<number>(-1)
+  const [duration, setDuration] = useState<number>(-1)
+  const [song, setSong] = useState<string>("")
+  const [isMuted, setMuted] = useState<boolean>(false)
+  const [status, setStatus] = useState<string>("paused")
 
-  const player = useRef<HTMLAudioElement>(new Audio)
-  let [currentTime, setCurrentTime] = useState<number>(-1)
-  let [duration, setDuration] = useState<number>(-1)
-  let [song, setSong] = useState<string>("")
-
-  player.current.oncanplaythrough = (e) => {
-    let audioObject = e.target as HTMLAudioElement
-    setCurrentTime(audioObject.currentTime)
-    setDuration(audioObject.duration)
+  player.current.oncanplaythrough = ({target}: Event) => {
+    let audioTarget = target as HTMLAudioElement
+    setCurrentTime(audioTarget.currentTime)
+    setDuration(audioTarget.duration)
   };
 
-  player.current.ontimeupdate = (e) => {
-    let audioObject = e.target as HTMLAudioElement
-    console.log(audioObject.currentTime)
+  player.current.ontimeupdate = ({target}: Event) => {
+    let audioObject = target as HTMLAudioElement
     setCurrentTime(audioObject.currentTime)
   }
 
-  const onPlay = () => {
-    player.current.play()
-  }
-  
-  const onPause = () => {
-    player.current.pause()
+  player.current.onended = ({target}: Event) => {
+    setStatus("paused")
   }
 
-  const onAudioSelect = async (url: string) => {
+  const onPlay = (): void => {
+    console.log(song)
+    if(song){
+      player.current.play()
+      setStatus("playing")
+    }
+    else{
+      alert("no song selected")
+    }
+  }
+
+  const onPause = (): void => {
+    player.current.pause()
+    setStatus("paused")
+  }
+
+  const onMute = (): void => {
+    setMuted(!isMuted)
+  }
+
+  const onAudioSelect = (url: string): void => {
     setSong(url)
+  }
+
+  const handleSlider = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    player.current.currentTime = parseInt(e.target.value)
   }
   
   return (
@@ -43,7 +67,7 @@ const MusicPlayer: React.FC = () => {
       <div>{!song ? "No song selected" : "Song selected"}</div>
       <button onClick={() => onAudioSelect(testsong)}>audio 1</button>
       <button onClick={() => onAudioSelect(testsong2)}>audio 2</button>
-      <audio ref={player} src={song} id="music-player"/>
+      <audio ref={player} src={song} id="music-player" muted={isMuted} preload={"auto"}/>
       <div style={{width: "100%", height: "100px", backgroundColor:"#393e46"}} className="d-flex flex-column align-items-center">
         <div className="d-flex flex-row"  style={{color:"#eeeeee"}}>
           <div>{currentTime >= 0 ? moment(currentTime*1000).format("mm:ss") : "--:--"}</div>
@@ -51,8 +75,11 @@ const MusicPlayer: React.FC = () => {
           <div>{duration >=0 ? moment(duration*1000).format("mm:ss") : "--:--"}</div>
         </div>
         <div className="d-flex flex-row">
-        <img src={playbutton} onClick={onPlay} className="player-button" alt="play button"/>
-        <img src={pausebutton} onClick={onPause} className="player-button" alt="pause button"/>
+          <input type="range" min={0} max={duration} value={currentTime} onChange={handleSlider} className="seeker"/>
+        {status === "paused" ? 
+        <img src={playbutton} onClick={onPlay} className="player-button" alt="play button"/> :        
+        <img src={pausebutton} onClick={onPause} className="player-button" alt="pause button"/>}
+        <button onClick={onMute}>mute</button>
         </div>
       </div>
     </div>
