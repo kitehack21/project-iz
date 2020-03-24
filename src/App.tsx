27b2a06d23665cc20861c8d382a9aa77996a71f2
Billy Irianto <billy_irianto@yahoo.com>
2020-03-24 23:10:36 +0700

    moved FA icon imports to a separate

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  faPlay,
-  faPause,
-  faFastBackward,
-  faFastForward,
-  faVolumeUp,
-  faVolumeDown,
-  faVolumeMute,
-  faVolumeOff,
-} from '@fortawesome/free-solid-svg-icons';
+
 import { Provider } from 'react-redux';
 import store from 'redux/store';
+import Sidebar from 'components/sidebar';
 import Page from './components/commons/Page';
 import Header from './components/header';
 import MusicPlayer from './components/MusicPlayer';
 import StickyBottom from './components/MusicPlayer/StickyBottom';
 import './bootstrap.css';
 import './App.css';
-
-library.add(
-  faPlay,
-  faPause,
-  faFastBackward,
-  faFastForward,
-  faVolumeUp,
-  faVolumeDown,
-  faVolumeMute,
-  faVolumeOff,
-);
+import './Icons';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="stripes" style={{ zIndex: -1 }} />
       <Page>
+        <Sidebar />
         <Header />
         <MusicPlayer />
       </Page>
---
+++ src/Icons.js
@@ -0,0 +1,22 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faPlay,
+  faPause,
+  faFastBackward,
+  faFastForward,
+  faVolumeUp,
+  faVolumeDown,
+  faVolumeMute,
+  faVolumeOff,
+} from '@fortawesome/free-solid-svg-icons';
+
+export default library.add(
+  faPlay,
+  faPause,
+  faFastBackward,
+  faFastForward,
+  faVolumeUp,
+  faVolumeDown,
+  faVolumeMute,
+  faVolumeOff,
+);
